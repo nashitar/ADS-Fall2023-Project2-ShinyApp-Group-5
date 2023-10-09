e947f25372a77fe563c40334b18a939f61dc5a7c
nashitar <52428369+nashitar@users.noreply.github.com>
2023-10-09 01:53:08 -0400

    Merged in clarence and nashita work

--- app/server.R
+++ app/server.R
@@ -1,9 +1,135 @@
-library(shiny)
-library(leaflet)
-library(leaflet.extras)
-library(glue)
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+###############################Install Related Packages #######################
+if (!require("shiny")) {
+  install.packages("shiny")
+  library(shiny)
+}
+if (!require("leaflet")) {
+  install.packages("leaflet")
+  library(leaflet)
+}
+if (!require("leaflet.extras")) {
+  install.packages("leaflet.extras")
+  library(leaflet.extras)
+}
+if (!require("dplyr")) {
+  install.packages("dplyr")
+  library(dplyr)
+}
+if (!require("magrittr")) {
+  install.packages("magrittr")
+  library(magrittr)
+}
+if (!require("mapview")) {
+  install.packages("mapview")
+  library(mapview)
+}
+if (!require("leafsync")) {
+  install.packages("leafsync")
+  library(leafsync)
+}
 
-shinyServer(function(input, output, session) {
+if (!require("readr")) {
+  install.packages("readr")
+  library(readr)
+}
+if (!require("stringr")) {
+  install.packages("stringr")
+  library(stringr)
+}
+if (!require("plotly")) {
+  install.packages("plotly")
+  library(plotly)
+}
+
+
+#Data Processing
+raw_data <- read_csv("../data/raw_dataset_FemaWebDeclarationAreas.csv") %>% filter_all(all_vars(!is.na(.)))
+raw_data2 <- read_csv("../data/raw_dataset_DisasterDeclarationsSummaries.csv") %>% filter_all(all_vars(!is.na(.)))
+# Group by the column and then summarize, Other, Terrorist, Typhoon are 3 categories that appear too less 
+# count_data <- raw_data2 %>%
+#   group_by(declarationType) %>%
+#   summarise(Count = n())
+# print(count_data)
+raw_data3 <- read_csv("../data/raw_dataset_DisasterDeclarationsSummaries.csv")
+
+
+# set.seed(123)  # Setting a seed ensures reproducibility of the random sample
+# subset_data <- raw_data %>% sample_n(1000)
+
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+  
+  
+  rv <- reactiveValues(update = 0)
+  observeEvent(input$StackedBCFilter, {
+    rv$update <- input$StackedBCFilter
+  })
+  
+  output$box <- renderPlot({
+    library(tidyverse)
+    print(rv$update)
+    
+    if (rv$update == 1) { 
+      ggplot(raw_data %>%
+               
+               # Extract the year from the entryDate
+               mutate(Year = substr(entryDate, 1, 4)) %>%
+               filter(stateName %in% c("California", "Texas", "Oklahoma")) %>%
+               
+               
+               # Group by the Year and stateName, and count the number of rows
+               group_by(Year, stateName) %>%
+               summarise(Total = n()) %>%
+               ungroup(),
+             
+             aes(x = Year, y = Total, fill = stateName), environment = environment()) +
+        geom_bar(position = "stack", stat = "identity") +
+        ylab("Total") +
+        labs(title = "Disaster Count Distribution of the top 3 States Over Time")
+    }
+    else if (rv$update == 2){
+      ggplot(raw_data2 %>%
+               
+               # Extract the year from the entryDate
+               mutate(Year = substr(declarationDate, 1, 4)) %>%
+               filter(!incidentType %in% c("Other", "Terrorist", "Typhoon")) %>%
+               
+               group_by(Year, incidentType) %>%
+               summarise(Total = n()) %>%
+               ungroup(),
+             
+             aes(x = Year, y = Total, fill = incidentType), environment = environment()) +
+        geom_bar(position = "stack", stat = "identity") +
+        ylab("Total") +
+        labs(title = "Disater Count Distribution of Incident Type Over Time")
+    }
+    else if (rv$update == 3){
+      ggplot(raw_data3 %>%
+               mutate(Year = substr(declarationDate, 1, 4)) %>%
+               
+               group_by(Year, declarationType) %>%
+               summarise(Total = n()) %>%
+               filter(Total > 1000) %>%
+               ungroup(),
+             
+             aes(x = Year, y = Total, fill = declarationType), environment = environment()) +
+        geom_bar(position = "stack", stat = "identity") +
+        ylab("Total") +
+        labs(title = "Disaster Count Distribution of Declaration Type Over Time")
+    }
+  })
+  
+  
+  ## Map Tab section
   
   # Hardcode the file paths and column names
   lat_col <- "latitude"
@@ -86,3 +212,5 @@ shinyServer(function(input, output, session) {
   })
   
 })
+
+
