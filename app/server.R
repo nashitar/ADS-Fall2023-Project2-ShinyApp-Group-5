dc5451917ffc53cdd444dc8fb5bde8cc9d51ae1f
Clarence <yunfan.jiang@emory.edu>
2023-10-10 01:12:16 -0400

    update for shiny app

--- app/server.R
+++ app/server.R
@@ -208,81 +208,81 @@ shinyServer(function(input, output) {
   lon_col <- "longitude"
   incident_type_col <- "incidentType"
   declaration_year_col <- "declarationYear"
-  
+
   # Create a reactive filter for declaration year
   filtered_data_year <- reactive({
     year_range <- input$year_filter
-    
+
     df <- data.frame()
     if (!is.null(year_range)) {
       for (i in year_range[1]:year_range[2]) {
-        filename <- glue('../out/DisasterDeclarationsSummaries{i}.csv')
+        filename <- glue('out/DisasterDeclarationsSummaries{i}.csv')
         temp <- read.csv(filename)
-        
+
         # Filter out rows with missing or invalid lat/lon values
         temp <- temp[!is.na(temp[[lat_col]]) & !is.na(temp[[lon_col]]), ]
-        
+
         df <- rbind(df, temp)
       }
     }
-    
+
     return(df)
   })
-  
+
   # Create a reactive filter for incident type
   filtered_data_incident_type <- reactive({
     incident_type <- input$incident_type_filter
-    
+
     df <- data.frame()
     if (incident_type != "All") {
-      filename <- glue('../out/DisasterDeclarationsSummaries{incident_type}.csv')
+      filename <- glue('/out/DisasterDeclarationsSummaries{incident_type}.csv')
       temp <- read.csv(filename)
-      
+
       # Filter out rows with missing or invalid lat/lon values
       temp <- temp[!is.na(temp[[lat_col]]) & !is.na(temp[[lon_col]]), ]
-      
+
       df <- rbind(df, temp)
     } else {
-      filename <- glue('../out/DisasterDeclarationsSummariesCleaned.csv')
+      filename <- glue('/out/DisasterDeclarationsSummariesCleaned.csv')
       temp <- read.csv(filename)
-      
+
       # Filter out rows with missing or invalid lat/lon values
       temp <- temp[!is.na(temp[[lat_col]]) & !is.na(temp[[lon_col]]), ]
-      
+
       df <- rbind(df, temp)
     }
-    
+
     return(df)
   })
-  
+
   # Render the map for year filter
   output$map_year <- renderLeaflet({
     data <- filtered_data_year()
-    
+
     leaflet() %>%
       addTiles() %>%
       addHeatmap(data = data,
                  lat = ~data[[lat_col]],
                  lng = ~data[[lon_col]],
-                 radius = 10,  
-                 intensity = 10,  
+                 radius = 10,
+                 intensity = 10,
                  blur = 10)
   })
-  
+
   # Render the map for incident type filter
   output$map_incident_type <- renderLeaflet({
     data <- filtered_data_incident_type()
-    
+
     leaflet() %>%
       addTiles() %>%
       addHeatmap(data = data,
                  lat = ~data[[lat_col]],
                  lng = ~data[[lon_col]],
-                 radius = 10,  
-                 intensity = 10,  
+                 radius = 10,
+                 intensity = 10,
                  blur = 10)
   })
-  
+
 })
 
 
--- app/ui.R
+++ app/ui.R
@@ -141,42 +141,40 @@ shinyUI(
                  )
                )
              )
-             
-             
              ,
-             
+
              tabPanel("Maps",
                       icon = icon("map-marker-alt"), #choose the icon for
                       div(class = 'outer',
                           # side by side plots
                           fluidPage(
-                            
-                            
+
+
                             # Create a tabsetPanel with two tabs
                             tabsetPanel(
                               tabPanel("Filter by Year",
-                                       sliderInput("year_filter", "Select Year Range", 
+                                       sliderInput("year_filter", "Select Year Range",
                                                    min = 1953,
                                                    max = 2023,
                                                    value = c(1953, 2023)),
                                        leafletOutput("map_year", height = 600, width = "100%")),
-                              
+
                               tabPanel("Filter by Incident Type",
-                                       selectInput("incident_type_filter", "Select Incident Type", 
-                                                   c('All', 'Fire', 'Flood', 'Hurricane', 'SevereStorm', 
-                                                     'WinterStorm', 'Tornado', 'Snowstorm', 'Earthquake', 
-                                                     'Biological', 'MudLandslide', 'CoastalStorm', 
-                                                     'Other', 'SevereIceStorm', 'DamLeveeBreak', 
-                                                     'TropicalStorm', 'Typhoon', 'VolcanicEruption', 
-                                                     'Freezing', 'ToxicSubstances', 'Chemical', 
-                                                     'Terrorist', 'Drought', 'HumanCause', 
+                                       selectInput("incident_type_filter", "Select Incident Type",
+                                                   c('All', 'Fire', 'Flood', 'Hurricane', 'SevereStorm',
+                                                     'WinterStorm', 'Tornado', 'Snowstorm', 'Earthquake',
+                                                     'Biological', 'MudLandslide', 'CoastalStorm',
+                                                     'Other', 'SevereIceStorm', 'DamLeveeBreak',
+                                                     'TropicalStorm', 'Typhoon', 'VolcanicEruption',
+                                                     'Freezing', 'ToxicSubstances', 'Chemical',
+                                                     'Terrorist', 'Drought', 'HumanCause',
                                                      'FishingLosses', 'Tsunami')),
                                        leafletOutput("map_incident_type", height = 600, width = "100%"))
                             )
                           ) #Panel Control - Closing
                       ) #Maps - Div closing
              ) #tabPanel maps closing
-             
+
              
              
   ) #navbarPage closing  
